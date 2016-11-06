6dacb8e9b92e11a4f9fec98e94d6c7fc1471719b
QUYNV <QUYNV@admins-MacBook-Pro-2.local>
2016-11-06 16:01:38 +0700

    fist commit

---
+++ NavController-TabbarController/BaseTabbarController.swift
@@ -0,0 +1,44 @@
+//
+//  BaseTabbarController.swift
+//  NavController-TabbarController
+//
+//  Created by Vinh The on 7/17/16.
+//  Copyright © 2016 Vinh The. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabbarController: UITabBarController, UITabBarControllerDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let bookVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("YourBookVC") as! YourBookController
+        
+        let homeVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("HomeVC") as! HomeViewController
+        
+        let profileVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileVC") as! ProfileViewController
+        
+        
+        let bookNav = BaseNavigationController(rootViewController: bookVC)
+        let homeNav = BaseNavigationController(rootViewController: homeVC)
+        let profileNav = BaseNavigationController(rootViewController: profileVC)
+        
+        viewControllers = [bookNav, homeNav, profileNav]
+        
+        profileVC.title = "Profile"
+        homeVC.title = "Home"
+        bookVC.title = "Book"
+        
+        
+    }
+    
+    
+    
+}
